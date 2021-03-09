817a3061b8828136b7b189d235ac080bd29c6982
Allen <fire0108@gmail.com>
2021-03-09 12:46:40 +0900

    domain

---
+++ src/main/java/com/jpabook/jpashop/domain/DeliverlyStatus.java
@@ -0,0 +1,7 @@
+package com.jpabook.jpashop.domain;
+
+public enum DeliverlyStatus {
+
+    READY, COMP
+
+}
---
+++ src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package com.jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")//나는 연관관계 주인이 아니에요, 나는 매핑된 거울일뿐이야 read only
+    private List<Order> orders = new ArrayList<>();
+}
